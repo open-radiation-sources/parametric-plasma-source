eede27b3a135644094a5a9f3a7c8047d8d83b924
shimwell <shimwell@yahoo.com>
2020-04-28 17:48:49 +0100

    added MIT lic

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     scripts=['parametric_plasma_source/'], #puts files in /usr/local/bin
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
