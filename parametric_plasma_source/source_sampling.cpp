21df5f29678b113b5e45a3362a3d9ea77f0d5b25
shimwell <shimwell@yahoo.com>
2020-04-28 17:46:15 +0100

    returned from $keywords$ to default exisiting code

--- parametric_plasma_source/source_sampling.cpp
+++ parametric_plasma_source/source_sampling.cpp
@@ -6,24 +6,24 @@
 
 
 // Spherical tokamak SOURCE 
-const double ion_density_pedistal = $ion_density_pedistal$; // ions per m^3
-const double ion_density_seperatrix =$ion_density_seperatrix$;
-const double ion_density_origin = $ion_density_origin$;
-const double ion_temperature_pedistal = $ion_temperature_pedistal$;
-const double ion_temperature_seperatrix = $ion_temperature_seperatrix$;
-const double ion_temperature_origin = $ion_temperature_origin$;
-const double pedistal_radius = $pedistal_radius$; // pedistal major rad
-const double ion_density_peaking_factor = $ion_density_peaking_factor$;
-const double ion_temperature_peaking_factor = $ion_temperature_peaking_factor$; // check alpha or beta value from paper
-const double minor_radius = $minor_radius$; // metres
-const double major_radius = $major_radius$; // metres
-const double elongation = $elongation$;
-const double triangularity = $triangularity$;
-const double shafranov_shift = $shafranov_shift$; //metres
+// units are in SI units
+const double ion_density_pedistal = 1.09e+20; // ions per m^3
+const double ion_density_seperatrix = 3e+19;
+const double ion_density_origin = 1.09e+20;
+const double ion_temperature_pedistal = 6.09;
+const double ion_temperature_seperatrix = 0.1;
+const double ion_temperature_origin = 45.9;
+const double pedistal_radius = 0.8; // pedistal major rad
+const double ion_density_peaking_factor = 1;
+const double ion_temperature_peaking_factor = 8.06; // check alpha or beta value from paper
+const double minor_radius = 1.56; // metres
+const double major_radius = 2.5; // metres
+const double elongation = 2.0;
+const double triangularity = 0.55;
+const double shafranov_shift = 0.0; //metres
 const std::string name = "parametric_plasma_source";
-const int number_of_bins  = $number_of_bins$;
-const int plasma_type = $plasma_type$; // 1 is default; //0 = L mode anything else H/A mode
-
+const int number_of_bins  = 100;
+const int plasma_type = 1; // 1 is default; //0 = L mode anything else H/A mode
 
 
 plasma_source::PlasmaSource source = plasma_source::PlasmaSource(ion_density_pedistal,
