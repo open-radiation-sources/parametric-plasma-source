70124e4fd488c3bfc7aa190d678f57fb21fef84e
shimwell <shimwell@yahoo.com>
2020-05-06 16:36:27 +0100

    removed script folder

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="parametric_plasma_source",
-    version="0.0.01",
+    version="0.0.2",
     author="Jonathan Shimwell",
     author_email="jonathan.shimwell@ukaea.uk",
     description="Parametric plasma source for fusion simulations in OpenMC",
@@ -13,7 +13,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/shimwell/parametric_plasma_source",
     packages=setuptools.find_packages(),
-    scripts=['parametric_plasma_source/'], #puts files in /usr/local/bin
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
