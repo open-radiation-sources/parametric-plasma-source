5b5e3d1f57813f1c452fbdecd0e76f34a70e8b39
dan <dan.short@ukaea.uk>
2020-07-06 16:36:42 +0100

    Install bindings with setup.py
    Include instructions in setup.py to compile the bindings with cmake.
    Specifically only for the plasma_source library, since we don't need
    the source_sampling library in Python.
    
    Update CMakeLists to avoid forcing the library output directory as this
    conflicts with the way setuptools etc. look for package artefacts. This
    means that the resulting source_sampling library is available in the
    build directory, unless the CMAKE_LIBRARY_OUTPUT_DIRECTORY is
    passed into the cmake configuration step.
    
    Introduce an __init__.py so that the modules can easily be loaded and
    found by setuptools.
    
    Cleanup .gitignore and add dist and egg paths.

--- setup.py
+++ setup.py
@@ -1,9 +1,69 @@
-import setuptools
+import os
+import subprocess
+import sys
+
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+
+
+class CMakeExtention(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            subprocess.check_output(["cmake", "--version"])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the "
+                               "following extentions: "
+                               ", ".join(e.name for e in self.extensions))
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name))
+        )
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+                      "-DPYTHON_EXECUTABLE=" + sys.executable]
+
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
+
+        cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+        build_args += ["--", "-j2"]
+        build_args += ["plasma_source"]
+
+        env = os.environ.copy()
+        env["CXXFLAGS"] = "{} -DVERSION_INFO=\\\"{}\\\"".format(
+            env.get("CXXFLAGS", ""),
+            self.distribution.get_version()
+        )
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args,
+            cwd=self.build_temp,
+            env=env
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args,
+            cwd=self.build_temp
+        )
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="parametric_plasma_source",
     version="0.0.6",
     author="Jonathan Shimwell",
@@ -12,7 +72,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shimwell/parametric_plasma_source",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    ext_modules=[CMakeExtention("parametric_plasma_source/plasma_source")],
+    cmdclass=dict(build_ext=CMakeBuild),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
