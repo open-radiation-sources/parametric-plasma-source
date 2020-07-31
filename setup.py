c8d61421943f9319c7ac577ad0ac3fae4157b6de
Dan Short <dan.short@ukaea.uk>
2020-07-31 15:32:34 +0100

    Strip out build files that are no longer needed
    As the functionality will be used via serialisation, this means that we
    won't need to recompile the C++ library.

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ import sys
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
-from parametric_plasma_source.build_python import build as build_python
-
 class CMakeExtention(Extension):
     def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
@@ -64,8 +62,6 @@ class CMakeBuild(build_ext):
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
-build_python(source_dir="parametric_plasma_source")
 
 setup(
     name="parametric_plasma_source",
