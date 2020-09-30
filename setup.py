3d306593d4fdf66409b74f9a10c61b1ead538116
Dan Short <dan.short@ukaea.uk>
2020-09-30 15:12:49 +0100

    Check TestPyPI and PyPI versions
    This should be more robust than just using git e.g. if two PRs are worked on
    simultaneously.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+import json
 import os
+import requests
 import subprocess
 import sys
 
@@ -59,25 +61,68 @@ class CMakeBuild(build_ext):
 
 
 def get_version(release_override="0.0.1"):
+    def get_last_version_root(last_version):
+        if ".post" in last_version or ".dev" in last_version:
+            last_version_root = ".".join(last_version.split(".")[:-1])
+        else:
+            last_version_root = last_version
+        return last_version_root
+
     cwd = os.path.dirname(os.path.realpath(__file__))
-    version = subprocess.check_output(
+    git_version = subprocess.check_output(
         ["git", "describe", "--always", "--tags"], stderr=None, cwd=cwd
     ).strip().decode("utf-8")
-    if "." not in version:
-        # Git doesn't know about a tag yet, so manually set a version and get the total
-        # number of commits
-        num_commits = subprocess.check_output(
-            ["git", "rev-list", "--count", "HEAD"], stderr=None, cwd=cwd
-        ).strip().decode("utf-8")
-        version = release_override + ".post" + num_commits
+
+    if "." not in git_version:
+        # Git doesn't know about a tag yet, so set the version root to release_override
+        version_root = release_override
     else:
-        if "-" in version:
-            # This commit doesn't correspond to an annotated tag, so mark it as post and
-            # get the number of commits since the last annotated tag
-            version_split = version.split("-")
-            tag_version = version_split[0]
-            num_commits = version_split[1]
-            version = tag_version + ".post" + num_commits
+        version_root = git_version.split("-")[0]
+
+    if "." not in git_version or "-" in git_version:
+        # This commit doesn't correspond to a tag, so mark it as post or dev
+        response = requests.get(
+            "https://test.pypi.org/pypi/parametric-plasma-source/json"
+        )
+        if response.status_code == 200:
+            # Response from TestPyPI was successful - get latest version and increment
+            last_version = json.loads(response.content)["info"]["version"]
+            last_version_root = get_last_version_root(last_version)
+
+            if last_version_root == version_root:
+                # We're still on the same released version, so increment the 'post'
+                post_count = 1
+                if "post" in last_version:
+                    post_index = last_version.rfind("post") + 4
+                    post_count = int(last_version[post_index:])
+                    post_count += 1
+                version = version_root + ".post" + str(post_count)
+            else:
+                response = requests.get(
+                    "https://pypi.org/pypi/parametric-plasma-source/json"
+                )
+                dev_count = 1
+                if response.status_code == 200:
+                    # Response from PyPI was successful - get dev version and increment
+                    last_version = json.loads(response.content)["info"]["version"]
+                    last_version_root = get_last_version_root(last_version)
+
+                    if last_version_root == version_root:
+                        if "dev" in last_version:
+                            dev_index = last_version.rfind("dev") + 3
+                            dev_count = int(last_version[dev_index:])
+                            dev_count += 1
+                version = version_root + ".dev" + str(dev_count)
+        else:
+            # Bad response from TestPyPI, so use git commits (requires git history)
+            # NOTE: May cause version clashes on mutliple branches - use test.pypi
+            # to avoid this.
+            num_commits = subprocess.check_output(
+                ["git", "rev-list", "--count", "HEAD"], stderr=None, cwd=cwd
+            ).strip().decode("utf-8")
+            version = release_override + ".post" + num_commits
+    else:
+        version = version_root
     return version
 
 
