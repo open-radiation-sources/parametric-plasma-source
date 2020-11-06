e38cb8fdcc340eef9b9f6991b348076efa8620e0
Dan Short <dan.short@ukaea.uk>
2020-11-06 14:37:12 +0000

    Use PEP517 standard to control build order

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils import dir_util
 import os
+import pybind11
 import subprocess
 import sys
 
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
 
@@ -21,25 +21,19 @@ class CMakeExtention(Extension):
 
 class CMakeBuild(build_ext):
     def run(self):
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "pybind11==2.6.0"])
         try:
             subprocess.check_output(["cmake", "--version"])
         except FileNotFoundError:
-            try:
-                subprocess.check_call([sys.executable, "-m", "pip", "install", "cmake==3.18.2"])
-            except OSError:
-                raise RuntimeError(
-                    "CMake must be installed to build the "
-                    "following extentions: "
-                    ", ".join(e.name for e in self.extensions)
-                )
+            raise RuntimeError(
+                "CMake must be installed to build the "
+                "following extentions: "
+                ", ".join(e.name for e in self.extensions)
+            )
 
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        import pybind11
-
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         if not extdir.endswith(os.path.sep):
             extdir += os.path.sep
@@ -48,7 +42,7 @@ class CMakeBuild(build_ext):
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
             "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=" + extdir,
             "-DPYTHON_EXECUTABLE=" + sys.executable,
-            "-DPYBIND11_PATH=" + os.path.abspath(os.path.dirname(pybind11.__file__))
+            "-DPYBIND11_PATH=" + pybind11.commands.DIR
         ]
 
         cfg = "Debug" if self.debug else "Release"
