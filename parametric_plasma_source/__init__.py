d356378c7ab8a4de94a5e9dadb9f08ff8e9f29ae
Dan Short <dan.short@ukaea.uk>
2020-11-06 15:57:30 +0000

    Version bump

--- parametric_plasma_source/__init__.py
+++ parametric_plasma_source/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "0.0.9.dev1"
+__version__ = "0.0.9.dev2"
 
 PLASMA_SOURCE_PATH = os.path.dirname(__file__)
 SOURCE_SAMPLING_PATH = os.sep.join([PLASMA_SOURCE_PATH, "source_sampling.so"])
