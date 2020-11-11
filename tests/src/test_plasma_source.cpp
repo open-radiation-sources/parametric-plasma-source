88c5bc3cef396be835894f39872fd7e4e1908ca2
Dan Short <dan.short@ukaea.uk>
2020-11-11 16:56:11 +0000

    Use header only Catch2

--- tests/src/test_plasma_source.cpp
+++ tests/src/test_plasma_source.cpp
@@ -1,6 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch2/catch_all.hpp>
-#include <iostream>
+#include "catch.hpp"
 #include "plasma_source.hpp"
 
 double ion_density_ped = 1.09e+20;
@@ -48,24 +47,24 @@ plasma_source::PlasmaSource source = plasma_source::PlasmaSource(
 );
 
 TEST_CASE( "Ion density is computed", "[source]" ) {
-    REQUIRE( source.ion_density(0.0) == Catch::Approx(ion_density_origin) );
-    REQUIRE( source.ion_density(0.2) == Catch::Approx(ion_density_ped) );
-    REQUIRE( source.ion_density(2.4) == Catch::Approx(1.00629067e20) );
-    REQUIRE( source.ion_density(minor_radius) == Catch::Approx(ion_density_sep) );
+    REQUIRE( source.ion_density(0.0) == Approx(ion_density_origin) );
+    REQUIRE( source.ion_density(0.2) == Approx(ion_density_ped) );
+    REQUIRE( source.ion_density(2.4) == Approx(1.00629067e20) );
+    REQUIRE( source.ion_density(minor_radius) == Approx(ion_density_sep) );
 }
 
 TEST_CASE( "Ion temperature is computed", "[source]" ) {
-    REQUIRE( source.ion_temperature(0.0) == Catch::Approx(ion_temp_origin) );
-    REQUIRE( source.ion_temperature(0.2) == Catch::Approx(45.89987429) );
-    REQUIRE( source.ion_temperature(2.4) == Catch::Approx(5.45529258) );
-    REQUIRE( source.ion_temperature(minor_radius) == Catch::Approx(ion_temp_sep) );
+    REQUIRE( source.ion_temperature(0.0) == Approx(ion_temp_origin) );
+    REQUIRE( source.ion_temperature(0.2) == Approx(45.89987429) );
+    REQUIRE( source.ion_temperature(2.4) == Approx(5.45529258) );
+    REQUIRE( source.ion_temperature(minor_radius) == Approx(ion_temp_sep) );
 }
 
 TEST_CASE( "D-T cross section is computed", "[source]" ) {
-    REQUIRE( source.dt_xs(ion_temp_origin) == Catch::Approx(8.14659e-22) );
-    REQUIRE( source.dt_xs(45.89987429) == Catch::Approx(8.14658e-22) );
-    REQUIRE( source.dt_xs(5.45529258) == Catch::Approx(1.80129e-23) );
-    REQUIRE( source.dt_xs(ion_temp_sep) == Catch::Approx(2.48478e-36) );
+    REQUIRE( source.dt_xs(ion_temp_origin) == Approx(8.14659e-22) );
+    REQUIRE( source.dt_xs(45.89987429) == Approx(8.14658e-22) );
+    REQUIRE( source.dt_xs(5.45529258) == Approx(1.80129e-23) );
+    REQUIRE( source.dt_xs(ion_temp_sep) == Approx(2.48478e-36) );
 }
 
 TEST_CASE( "Source sampling", "[source]" ) {
@@ -74,11 +73,11 @@ TEST_CASE( "Source sampling", "[source]" ) {
     double e;
     std::array<double, 8> rands = {0.2, 0.3, 0.1, 0.2, 0.3, 0.1, 0.2, 0.3};
     source.sample(rands, x, y, z, u, v, w, e);
-    REQUIRE( x == Catch::Approx(9.2401323) );
-    REQUIRE( y == Catch::Approx(3.0023) );
-    REQUIRE( z == Catch::Approx(0.275493) );
-    REQUIRE( u == Catch::Approx(0.283219) );
-    REQUIRE( v == Catch::Approx(0.871658) );
-    REQUIRE( w == Catch::Approx(0.4) );
-    REQUIRE( e == Catch::Approx(14.7198) );
+    REQUIRE( x == Approx(9.2401323) );
+    REQUIRE( y == Approx(3.0023) );
+    REQUIRE( z == Approx(0.275493) );
+    REQUIRE( u == Approx(0.283219) );
+    REQUIRE( v == Approx(0.871658) );
+    REQUIRE( w == Approx(0.4) );
+    REQUIRE( e == Approx(14.7198) );
 }
