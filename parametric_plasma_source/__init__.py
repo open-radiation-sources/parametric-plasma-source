3fa477303d9ff47e4e5d7bcd5efc06d958f4aecc
Andrew Davis <andrew.davis@ukaea.uk>
2023-01-31 21:20:39 +0000

    bump version number

--- parametric_plasma_source/__init__.py
+++ parametric_plasma_source/__init__.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import warnings
 
-__version__ = "0.0.9.dev4"
+__version__ = "0.0.10.dev"
 
 PLASMA_SOURCE_PATH = os.path.dirname(__file__)
 SOURCE_SAMPLING_PATH = os.sep.join([PLASMA_SOURCE_PATH, "source_sampling.so"])
